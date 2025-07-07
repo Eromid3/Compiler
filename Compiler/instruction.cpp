1c1842e882cc972378b8d7446281381efafd21b9
Arkadiusz Treder <eromid3@gmail.com>
2025-07-07 23:05:24 +0200

    Completed AssignmentInstruction and evaluateExpression

--- Compiler/instruction.cpp
+++ Compiler/instruction.cpp
@@ -30,7 +30,11 @@ string getNextToken(string& expression)
     return result;
 }
 
-int evaluateExpression(string expression, std::unordered_map<std::string, int> variables)
+bool isOp(string token) {
+    return token == "+" || token == "-" || token == "*" || token == "/";
+}
+
+vector<string> makeRPN(string expression)
 {
     skipWhitespace(expression);
     stack<char> ops;
@@ -39,22 +43,98 @@ int evaluateExpression(string expression, std::unordered_map<std::string, int> v
     while (expression != "")
     {
         token = getNextToken(expression);
+        if (isOp(token))
+        {
+            int priority = getPriority(token[0]);
+            while (!ops.empty())
+            {
+                char top = ops.top();
+                ops.pop();
+                if (top == '(' or getPriority(top) < priority)
+                {
+                    ops.push(top);
+                    break;
+                }
+                output.push_back(string(1, top));
+            }
+            ops.push(token[0]);
+        }
+        else if (token == "(") ops.push(token[0]);
+        else if (token == ")")
+        {
+            while (!ops.empty())
+            {
+                token = ops.top();
+                ops.pop();
+                if (token == "(") break;
+                else output.push_back(token);
+            }
+        }
+        else output.push_back(token);
+    }
+    while (!ops.empty())
+    {
+        token = ops.top();
+        ops.pop();
+        output.push_back(token);
     }
 
+    return output;
 }
 
-void AssignmentInstruction::execute(std::unordered_map<std::string, int> variables)
+int evaluateExpression(string expression, std::unordered_map<std::string, int> variables)
 {
-	int value = stoi(right);
+    vector<string> rpn = makeRPN(expression);
+    stack<int> values;
+    for (string& token : rpn)
+    {
+        if (isOp(token))
+        {
+            if (values.size() < 2) throw runtime_error("Invalid expression");
+
+            int b = values.top(); values.pop();
+            int a = values.top(); values.pop();
+
+            int result = 0;
+            if (token == "+") result = a + b;
+            else if (token == "-") result = a - b;
+            else if (token == "*") result = a * b;
+            else if (token == "/") result = a / b;
+
+            values.push(result);
+        }
+        else
+        {
+            int value;
+            if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1)) {
+                value = stoi(token);
+            }
+            else {
+                if (variables.find(token) == variables.end())
+                    throw runtime_error("Undefined variable: " + token);
+                value = variables[token];
+            }
+            values.push(value);
+        }
+    }
+
+    if (values.size() != 1) throw runtime_error("Invalid expression result");
+
+    return values.top();
+}
+
+void AssignmentInstruction::execute(std::unordered_map<std::string, int>& variables)
+{
+    int value = evaluateExpression(right, variables);
 	variables[left] = value;
 }
 
-void IfInstruction::execute(std::unordered_map<std::string, int> variables)
+void IfInstruction::execute(std::unordered_map<std::string, int>& variables)
 {
 
 }
 
-void GotoInstruction::execute(std::unordered_map<std::string, int> variables)
+void GotoInstruction::execute(std::unordered_map<std::string, int>& variables)
 {
 
 }
--- Compiler/instruction.h
+++ Compiler/instruction.h
@@ -5,22 +5,22 @@
 
 struct Instruction {
     virtual ~Instruction() = default;
-    virtual void execute(std::unordered_map<std::string, int> variables) = 0;
+    virtual void execute(std::unordered_map<std::string, int>& variables) = 0;
 };
 
 struct AssignmentInstruction : Instruction {
     std::string left;
     std::string right;
-    void execute(std::unordered_map<std::string, int> variables) override;
+    void execute(std::unordered_map<std::string, int>& variables) override;
 };
 
 struct IfInstruction : Instruction {
     std::string condition;
     std::vector<std::unique_ptr<Instruction>> block;
-    void execute(std::unordered_map<std::string, int> variables) override;
+    void execute(std::unordered_map<std::string, int>& variables) override;
 };
 
 struct GotoInstruction : Instruction {
     std::string tag;
-    void execute(std::unordered_map<std::string, int> variables) override;
+    void execute(std::unordered_map<std::string, int>& variables) override;
 };
